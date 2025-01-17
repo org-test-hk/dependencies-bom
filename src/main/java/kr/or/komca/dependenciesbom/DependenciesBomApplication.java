f55006cd96e2c8f95926faf03d58d18eb7560a85
Kim Munjin <reminisce057@gmail.com>
2025-01-17 15:52:27 +0900

    init: 초기 설정

---
+++ src/main/java/kr/or/komca/dependenciesbom/DependenciesBomApplication.java
@@ -0,0 +1,13 @@
+package kr.or.komca.dependenciesbom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependenciesBomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependenciesBomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/or/komca/dependenciesbom/DependenciesBomApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.komca.dependenciesbom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependenciesBomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
