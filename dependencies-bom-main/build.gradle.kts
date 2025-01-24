fe60955218996bb46576c0833bece0b3b85edaed
kun <sus5330@icloud.com>
2025-01-24 16:52:58 +0900

    refactor: update data-core, verification, logging

---
+++ dependencies-bom-main/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("maven-publish")
+    id("java-platform")
+}
+
+group = "kr.or.komca"
+version = "0.0.1-SNAPSHOT"
+
+
+javaPlatform {
+    allowDependencies()
+}
+
+
+
+dependencies {
+    constraints {
+        api("kr.or.komca:komca-data-core:0.2.6")
+        api("kr.or.komca:utils:0.3.2")
+        api("kr.or.komca.foundation:verification:0.5.18")
+        api("kr.or.komca:common-logging:0.1.1")
+        api("kr.or.komca:exception:0.0.3")
+    }
+}
+
+
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/${System.getenv("GITHUB_REPOSITORY")}")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("bom") {
+            from(components["javaPlatform"])
+            groupId = "kr.or.komca"
+            artifactId = "dependencies-bom"
+        }
+    }
+}
+
