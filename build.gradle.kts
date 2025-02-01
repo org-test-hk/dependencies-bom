24d141adb94c3a8ce827c8768c2cdeed5d5b929c
Kim Munjin <reminisce057@gmail.com>
2025-02-01 13:55:10 +0900

    refactor: jwt-verification 버전 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
 
         api("kr.or.komca:komca-data-core:0.2.7")
         api("kr.or.komca:utils:0.3.2")
-        api("kr.or.komca.foundation:verification:0.5.19")
+        api("kr.or.komca.foundation:verification:0.5.20")
         api("kr.or.komca:common-logging:0.1.1")
 
         api("kr.or.komca:exception:0.0.4")
