3e5057dbbada6178345cc936a19d16f55d4420d3
Kim Munjin <reminisce057@gmail.com>
2025-02-02 13:32:28 +0900

    refactor: build.gradle.kts 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,10 @@ javaPlatform {
 dependencies {
     constraints {
 
-        api("kr.or.komca:komca-data-core:0.2.7")
+        api("kr.or.komca:komca-data-core:0.2.8")
         api("kr.or.komca:utils:0.3.2")
         api("kr.or.komca.foundation:verification:0.5.21")
-        api("kr.or.komca:common-logging:0.1.1")
+        api("kr.or.komca:common-logging:0.1.2")
         api("kr.or.komca:exception:0.0.5")
     }
 }
