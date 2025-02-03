7ad713b832747fe32cb43151b2e66bc2c09bd2cb
kun <sus5330@icloud.com>
2025-02-03 18:40:26 +0900

    refactor: 공통 응답 추상 클래스 리팩토링 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
 
         api("kr.or.komca:komca-data-core:0.3.1")
         api("kr.or.komca:utils:0.3.2")
-        api("kr.or.komca.foundation:verification:0.5.18")
+        api("kr.or.komca.foundation:verification:0.5.23")
         api("kr.or.komca:common-logging:0.1.1")
         api("kr.or.komca:exception:0.0.6")
     }
