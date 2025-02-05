c2da0da268560bed77a9c9c557ab1a5e47c85700
kun <sus5330@icloud.com>
2025-02-05 12:31:15 +0900

    update: 버전 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ javaPlatform {
 dependencies {
     constraints {
 
-        api("kr.or.komca:komca-data-core:0.3.3")
+        api("kr.or.komca:komca-data-core:0.3.4")
         api("kr.or.komca:utils:0.3.2")
         api("kr.or.komca.foundation:verification:0.5.24")
         api("kr.or.komca:common-logging:0.1.3")
